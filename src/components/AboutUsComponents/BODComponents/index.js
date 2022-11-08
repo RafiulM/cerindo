5056ae864397e454b4317d9de0dd546ea978cdd0
Rafiul <ahmadrafiulm@gmail.com>
2022-11-08 08:32:20 +0700

    BoD + 1 CHCO

--- src/components/AboutUsComponents/BODComponents/index.js
+++ src/components/AboutUsComponents/BODComponents/index.js
@@ -106,7 +106,7 @@ const BODComponents = () => {
             </TheKing>
             <BodGrid>
               {BOD.listBoardOfDirectors.map((data, idx) => {
-                if (idx < 5 && idx > 0) {
+                if (idx < 6 && idx > 0) {
                   return (
                     <Bodbox key={idx}>
                       <BodBoxContent>
@@ -209,7 +209,7 @@ const BODComponents = () => {
             <BODTitle>{BODMTitleLang(BODMTitleData, language,1)}</BODTitle>
             <BodGrid>
               {BOD.listBoardOfDirectors.map((data, idx) => {
-                if (idx > 4) {
+                if (idx > 5) {
                   return (
                     <Bodbox key={idx}>
                       <BodBoxContent>
